841fabf0ccec38c2ed4ad1c105633e0c5b63ddbe
konradoboza <konrad.oboza@ibexa.co>
2023-12-12 13:37:02 +0100

    added `image_extensions` to the FieldType forms

--- src/lib/Form/Type/FieldType/ImageFieldType.php
+++ src/lib/Form/Type/FieldType/ImageFieldType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\Mime\MimeTypesInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -22,6 +23,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class ImageFieldType extends AbstractType
 {
+    private MimeTypesInterface $mimeTypes;
+
+    public function __construct(MimeTypesInterface $mimeTypes)
+    {
+        $this->mimeTypes = $mimeTypes;
+    }
+
     public function getName()
     {
         return $this->getBlockPrefix();
@@ -59,6 +67,7 @@ class ImageFieldType extends AbstractType
         $view->vars += [
             'is_alternative_text_required' => $options['is_alternative_text_required'],
             'mime_types' => $options['mime_types'],
+            'image_extensions' => $this->getMimeTypesExtensions($options['mime_types']),
         ];
     }
 
@@ -68,10 +77,27 @@ class ImageFieldType extends AbstractType
             'translation_domain' => 'ibexa_content_forms_fieldtype',
             'is_alternative_text_required' => false,
             'mime_types' => [],
+            'image_extensions' => [],
         ]);
 
         $resolver->setAllowedTypes('is_alternative_text_required', 'bool');
         $resolver->setAllowedTypes('mime_types', ['array']);
+        $resolver->setAllowedTypes('image_extensions', ['array']);
+    }
+
+    /**
+     * @param array<string> $mimeTypes
+     *
+     * @return array<string, array<string>>
+     */
+    private function getMimeTypesExtensions(array $mimeTypes): array
+    {
+        $extensions = [];
+        foreach ($mimeTypes as $mimeType) {
+            $extensions[$mimeType] = $this->mimeTypes->getExtensions($mimeType);
+        }
+
+        return $extensions;
     }
 }
 
