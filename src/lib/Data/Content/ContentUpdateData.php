cadd8c3f9d72ed17bad650f17c2c9ceb8f904ffa
Adam Wójs <adam@wojs.pl>
2024-01-09 21:09:58 +0100

    IBX-7492: Fixed error when editing image in user profile

--- src/lib/Data/Content/ContentUpdateData.php
+++ src/lib/Data/Content/ContentUpdateData.php
@@ -8,14 +8,16 @@ declare(strict_types=1);
 
 namespace Ibexa\ContentForms\Data\Content;
 
+use EzSystems\EzPlatformContentForms\Data\VersionInfoAwareInterface;
 use Ibexa\ContentForms\Data\NewnessCheckable;
 use Ibexa\Core\Repository\Values\Content\ContentUpdateStruct;
+use Ibexa\Core\Repository\Values\Content\VersionInfo;
 
 /**
  * @property \Ibexa\Contracts\ContentForms\Data\Content\FieldData[] $fieldsData
  * @property \Ibexa\Contracts\Core\Repository\Values\Content\Content $contentDraft
  */
-class ContentUpdateData extends ContentUpdateStruct implements NewnessCheckable
+class ContentUpdateData extends ContentUpdateStruct implements NewnessCheckable, VersionInfoAwareInterface
 {
     use ContentData;
 
@@ -24,6 +26,11 @@ class ContentUpdateData extends ContentUpdateStruct implements NewnessCheckable
     public function isNew()
     {
         return false;
+    }
+
+    public function getVersionInfo(): VersionInfo
+    {
+        return $this->contentDraft->versionInfo;
     }
 }
 
