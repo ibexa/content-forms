2d9166c4919f65b55f6d55a7c750a78f727552c0
Adam Wójs <adam@wojs.pl>
2024-01-10 15:05:11 +0100

    fixup! IBX-7492: Fixed error when editing image in user profile

--- src/lib/Data/User/UserUpdateData.php
+++ src/lib/Data/User/UserUpdateData.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 namespace Ibexa\ContentForms\Data\User;
 
 use Ibexa\ContentForms\Data\Content\ContentData;
-use Ibexa\ContentForms\Data\Content\VersionInfoAwareInterface;
+use Ibexa\ContentForms\Data\VersionInfoAwareInterface;
 use Ibexa\ContentForms\Data\NewnessCheckable;
 use Ibexa\Contracts\Core\Repository\Values\User\UserUpdateStruct;
 use Ibexa\Core\Repository\Values\Content\VersionInfo;
