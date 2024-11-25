06f27c00d1a9e1cdd2aed4c53e1778f31f774455
Andrew Longosz <alongosz@users.noreply.github.com>
2024-11-25 14:22:21 +0100

    [Tests] Aligned form mapper tests with Symfony 6

--- tests/lib/FieldType/Mapper/BaseMapperTest.php
+++ tests/lib/FieldType/Mapper/BaseMapperTest.php
@@ -11,53 +11,44 @@ namespace Ibexa\Tests\ContentForms\FieldType\Mapper;
 use Ibexa\Contracts\ContentForms\Data\Content\FieldData;
 use Ibexa\Contracts\Core\Repository\FieldType;
 use Ibexa\Contracts\Core\Repository\FieldTypeService;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormConfigInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 
 abstract class BaseMapperTest extends TestCase
 {
-    protected $fieldTypeService;
+    protected FieldTypeService & MockObject $fieldTypeService;
 
-    protected $config;
+    protected FormConfigInterface & MockObject $config;
 
-    protected $fieldForm;
+    protected FormInterface & MockObject $fieldForm;
 
-    protected $data;
+    protected FieldData & MockObject $data;
 
     protected function setUp(): void
     {
         $this->fieldTypeService = $this->getMockBuilder(FieldTypeService::class)
             ->getMock();
         $this->fieldTypeService
-            ->expects(self::any())
             ->method('getFieldType')
             ->willReturn($this->getMockBuilder(FieldType::class)->getMock());
 
         $this->config = $this->getMockBuilder(FormConfigInterface::class)->getMock();
 
-        $formFactory = $this->getMockBuilder(FormFactoryInterface::class)
-            ->setMethods(['addModelTransformer', 'setAutoInitialize', 'getForm'])
-            ->getMockForAbstractClass();
+        $this->fieldForm = $this->createMock(FormInterface::class);
+        $formBuilder = $this->createMock(FormBuilder::class);
+        $formFactory = $this->createMock(FormFactoryInterface::class);
         $formFactory->expects(self::once())
             ->method('createBuilder')
-            ->willReturn($formFactory);
-        $formFactory->expects(self::once())
-            ->method('create')
-            ->willReturn($formFactory);
-        $formFactory->expects(self::once())
-            ->method('addModelTransformer')
-            ->willReturn($formFactory);
-        $formFactory->expects(self::once())
-            ->method('setAutoInitialize')
-            ->willReturn($formFactory);
+            ->willReturn($formBuilder);
 
         $this->config->expects(self::once())
             ->method('getFormFactory')
             ->willReturn($formFactory);
 
-        $this->fieldForm = $this->getMockBuilder(FormInterface::class)->getMock();
         $this->fieldForm->expects(self::once())
             ->method('getConfig')
             ->willReturn($this->config);
