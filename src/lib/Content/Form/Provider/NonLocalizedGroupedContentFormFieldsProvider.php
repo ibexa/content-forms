d53ab2257b81f6b1d04a3953a3f0dd8ecbe9475e
Sztig <sebastianjamula@gmail.com>
2025-01-20 14:34:33 +0100

    implemented new provider with correct implementation of the interface to avoid BC break

---
+++ src/lib/Content/Form/Provider/NonLocalizedGroupedContentFormFieldsProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\ContentForms\Content\Form\Provider;
+
+use Ibexa\Contracts\ContentForms\Content\Form\Provider\GroupedContentFormFieldsProviderInterface;
+use Ibexa\Core\Helper\FieldsGroups\FieldsGroupsList;
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+
+class NonLocalizedGroupedContentFormFieldsProvider implements GroupedContentFormFieldsProviderInterface, TranslationContainerInterface
+{
+    /** @var \Ibexa\Core\Helper\FieldsGroups\FieldsGroupsList */
+    private $fieldsGroupsList;
+
+    public function __construct(FieldsGroupsList $fieldsGroupsList)
+    {
+        $this->fieldsGroupsList = $fieldsGroupsList;
+    }
+
+    public function getGroupedFields(array $fieldsDataForm): array
+    {
+        $groupedFields = [];
+
+        foreach ($fieldsDataForm as $fieldForm) {
+            /** @var \Ibexa\Contracts\ContentForms\Data\Content\FieldData $fieldData */
+            $fieldData = $fieldForm->getViewData();
+            $fieldGroupIdentifier = $this->fieldsGroupsList->getFieldGroup($fieldData->fieldDefinition);
+            $groupedFields[$fieldGroupIdentifier][] = $fieldForm->getName();
+        }
+
+        return $groupedFields;
+    }
+
+    public static function getTranslationMessages(): array
+    {
+        return [
+            Message::create('content', 'ibexa_fields_groups')->setDesc('Content'),
+            Message::create('metadata', 'ibexa_fields_groups')->setDesc('Metadata'),
+        ];
+    }
+}
