3b67a77f07e1718f064dca8c40d68c1a151d3a89
Adam Wójs <adam@wojs.pl>
2026-01-07 08:42:48 +0100

    IBX-7492: Fixed error when editing image in user profile (#60)
    * IBX-7492: Fixed error when editing image in user profile
    
    * fixup! IBX-7492: Fixed error when editing image in user profile
    
    * fixup! IBX-7492: Fixed error when editing image in user profile
    
    * fixup! IBX-7492: Fixed error when editing image in user profile
    
    * fixup! IBX-7492: Fixed error when editing image in user profile

--- src/lib/Data/Content/ContentUpdateData.php
+++ src/lib/Data/Content/ContentUpdateData.php
@@ -9,13 +9,15 @@ declare(strict_types=1);
 namespace Ibexa\ContentForms\Data\Content;
 
 use Ibexa\ContentForms\Data\NewnessCheckable;
+use Ibexa\ContentForms\Data\VersionInfoAwareInterface;
 use Ibexa\Core\Repository\Values\Content\ContentUpdateStruct;
+use Ibexa\Core\Repository\Values\Content\VersionInfo;
 
 /**
  * @property \Ibexa\Contracts\ContentForms\Data\Content\FieldData[] $fieldsData
  * @property \Ibexa\Contracts\Core\Repository\Values\Content\Content $contentDraft
  */
-class ContentUpdateData extends ContentUpdateStruct implements NewnessCheckable
+class ContentUpdateData extends ContentUpdateStruct implements NewnessCheckable, VersionInfoAwareInterface
 {
     use ContentData;
 
@@ -24,6 +26,11 @@ class ContentUpdateData extends ContentUpdateStruct implements NewnessCheckable
     public function isNew()
     {
         return false;
+    }
+
+    public function getVersionInfo(): VersionInfo
+    {
+        return $this->contentDraft->versionInfo;
     }
 }
 
