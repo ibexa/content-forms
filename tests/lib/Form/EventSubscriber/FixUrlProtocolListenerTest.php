12c80177053a2c570fca0858bd877bc66fea03ee
Thorsten Reiter <thorsten.reiter@ibexa.co>
2026-01-13 20:52:32 +0100

    Minor fix

--- tests/lib/Form/EventSubscriber/FixUrlProtocolListenerTest.php
+++ tests/lib/Form/EventSubscriber/FixUrlProtocolListenerTest.php
@@ -52,66 +52,57 @@ final class FixUrlProtocolListenerTest extends TestCase
      */
     public static function provideUrlCases(): iterable
     {
-        yield 'adds http when protocol missing' => [
-            ['link' => self::DOMAIN],
-            ['link' => self::URL_HTTP],
-        ];
-
-        yield 'does not modify https url' => [
-            ['link' => self::URL_HTTPS],
-            ['link' => self::URL_HTTPS],
-        ];
-
-        yield 'does not modify http url' => [
-            ['link' => self::URL_HTTP],
-            ['link' => self::URL_HTTP],
-        ];
-
-        yield 'keep relative url with leading / intact' => [
-            ['link' => self::URL_RELATIVE],
-            ['link' => self::URL_RELATIVE],
-        ];
-
-        yield 'keeps ftp intact' => [
-            ['link' => self::URL_SFTP],
-            ['link' => self::URL_SFTP],
-        ];
-
-        yield 'keeps tel intact' => [
-            ['link' => self::URL_TEL],
-            ['link' => self::URL_TEL],
-        ];
-
-        yield 'adds default tel' => [
-            ['link' => self::TEL],
-            ['link' => self::URL_TEL],
-            'tel',
-        ];
-
-        yield 'keeps mailto intact' => [
-            ['link' => self::URL_MAILTO],
-            ['link' => self::URL_MAILTO],
-        ];
-
-        yield 'adds default mailto' => [
-            ['link' => self::MAIL],
-            ['link' => self::URL_MAILTO],
-            'mailto',
-        ];
-
-        yield 'does nothing when link is empty string' => [
-            ['link' => ''],
-            ['link' => ''],
-        ];
-
-        yield 'does nothing when link key is missing' => [
-            [],
-            [],
-        ];
-
-        yield 'does nothing when data is null' => [
-            null,
-            null,
+        return [
+            'adds http when protocol missing' => [
+                ['link' => self::DOMAIN],
+                ['link' => self::URL_HTTP],
+            ],
+            'does not modify https url' => [
+                ['link' => self::URL_HTTPS],
+                ['link' => self::URL_HTTPS],
+            ],
+            'does not modify http url' => [
+                ['link' => self::URL_HTTP],
+                ['link' => self::URL_HTTP],
+            ],
+            'keep relative url with leading / intact' => [
+                ['link' => self::URL_RELATIVE],
+                ['link' => self::URL_RELATIVE],
+            ],
+            'keeps ftp intact' => [
+                ['link' => self::URL_SFTP],
+                ['link' => self::URL_SFTP],
+            ],
+            'keeps tel intact' => [
+                ['link' => self::URL_TEL],
+                ['link' => self::URL_TEL],
+            ],
+            'adds default tel' => [
+                ['link' => self::TEL],
+                ['link' => self::URL_TEL],
+                'tel',
+            ],
+            'keeps mailto intact' => [
+                ['link' => self::URL_MAILTO],
+                ['link' => self::URL_MAILTO],
+            ],
+            'adds default mailto' => [
+                ['link' => self::MAIL],
+                ['link' => self::URL_MAILTO],
+                'mailto',
+            ],
+            'does nothing when link is empty string' => [
+                ['link' => ''],
+                ['link' => ''],
+            ],
+            'does nothing when link key is missing' => [
+                [],
+                [],
+            ],
+            'does nothing when data is null' => [
+                null,
+                null,
+            ],
         ];
     }
 }
