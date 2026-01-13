dfa5566c02f6e2d5f073e2bd40ca7494a35c46d0
Thorsten Reiter <thorsten.reiter@ibexa.co>
2026-01-13 11:17:47 +0100

    IBX-10978: Added tel/mailto options to urlType

---
+++ src/lib/Form/EventSubscriber/FixUrlProtocolListener.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\ContentForms\Form\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class FixUrlProtocolListener implements EventSubscriberInterface
+{
+    private $defaultProtocol;
+
+    /**
+     * @param string|null $defaultProtocol The URL scheme to add when there is none or null to not modify the data
+     */
+    public function __construct(?string $defaultProtocol = 'http')
+    {
+        $this->defaultProtocol = $defaultProtocol;
+    }
+
+    public function onSubmit(FormEvent $event)
+    {
+        $data = $event->getData();
+        if (null === $this->defaultProtocol || empty($data) || !\is_string($data)) {
+            return;
+        }
+
+        $protocol = explode(':', $data)[0];
+        if ($this->hasAuthority($protocol) && preg_match('~^(?:[/.]|[\w+.-]+://|[^:/?@#]++@)~', $data)) {
+            return;
+        }
+
+        if (!$this->hasAuthority($protocol) && preg_match('~^(?:[/.]|[\w+.-]+:|[^:/?@#]++@)~', $data)) {
+            return;
+        }
+
+        $schemaSeparator = $this->hasAuthority($this->defaultProtocol) ? '://' : ':';
+        if (!preg_match('~^(?:[/.]|[\w+.-]+' . $schemaSeparator . '|[^:/?@#]++@)~', $data)) {
+            $event->setData($this->defaultProtocol . $schemaSeparator . $data);
+        }
+    }
+
+    private function hasAuthority($protocol)
+    {
+        return in_array($protocol, ['mailto', 'tel']) ? false : true;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [FormEvents::SUBMIT => 'onSubmit'];
+    }
+}
